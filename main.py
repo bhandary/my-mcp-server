c0f04d1513451c5638a51ce2cdb918a5616e4409
Manju Bhandary <bhandary@yahoo.com>
2026-01-17 20:49:37 -0800

    Update tools

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import os
 from mcp.server.fastmcp import FastMCP
 
-mcp = FastMCP(name="Tool Example")
+mcp = FastMCP(
+    "Tool Example",
+    host="0.0.0.0",
+)
 
 @mcp.tool()
 def sum(a: int, b: int) -> int:
@@ -14,9 +17,4 @@ def get_weather(city: str, unit: str = "celsius") -> str:
     return f"Weather in {city}: 22degrees{unit[0].upper()}"
 
 if __name__ == "__main__":
-    import uvicorn
-    # Get the port from environment variable
-    port = int(os.environ.get("PORT", 8000))
-    
-    # Run the server programmatically
-    mcp.run(transport="streamable-http", host="0.0.0.0", port=port)
+    mcp.run(transport='streamable-http')
